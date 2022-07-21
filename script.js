df3c7c5a04d08edfe6f2262161e71739fd4af091
Varun Chaubey <varunchaubey4@gmail.com>
2022-07-22 03:01:08 +0530

    Animation part got resolved and app is complete

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 var inputNumberTag = document.getElementById('input-number');
 var oldCountTag = document.getElementById('old-count');
 var newCountTag = document.getElementById('new-count');
-
+oldCountTag.innerHTML = '0';
 var inputVar;
 var count = 0;
-var newVal;
+
 function resultFunction(){
     if(inputNumberTag.value > 9 || inputNumberTag.value < 0)
     {
         alert('Number provided is either greater than 9 or less than 0');
     }
-    inputVar = inputNumberTag.value;
-    var interval = setInterval(animate,1000, count, inputVar);
-};
-
-function animate(){
-    if(count > inputVar - 1){
-        return;
+    else{
+        inputVar = inputNumberTag.value;
+        var interval = setInterval(function(){
+            if(count > inputVar-1){
+                alert('Counter has stopped');
+                count = 0;
+                inputVar = 0;
+                oldCountTag.innerHTML = 0;
+                clearInterval(interval);
+            }
+            else{
+                newCountTag.classList.add('animate');
+                setTimeout(function(){
+                    newCountTag.classList.remove('animate');
+                    oldCountTag.innerHTML = count;
+                }, 900);
+                count++;
+                newCountTag.innerHTML = count;
+            }
+        }, 1000);
     }
-    newCountTag.classList.add('animate');
-    setTimeout(function(){
-        newCountTag.classList.remove('animate');
-    }, 900);
-    count++;
-}
+};
